2056edd350b24ccb80a0cd79b0ff8d677b969bdf
Yuri Hallan <yuri.souza@pmm.am.gov.br>
2019-06-18 09:17:11 -0400

    Primeiro projeot feito no React - Canal Programador BR.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+import Conversor from './Components/Conversor';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Conversor de Moeda</h1>
+     
+        <div className="linha">
+          <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+          <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+        </div >  
+        <div className="linha">
+          <Conversor moedaA="CAD" moedaB="BRL"></Conversor>
+          <Conversor moedaA="BRL" moedaB="CAD"></Conversor>
+        </div>  
+      </div>  
+
+
+    
   );
 }
 
